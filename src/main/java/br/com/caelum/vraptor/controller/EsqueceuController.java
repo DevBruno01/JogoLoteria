c2ef56cf136a0c64113793f20c8355413077ae07
Bruno Brocanelli <bbrocanelli@gmail.com>
2022-02-28 21:21:46 -0300

    Primeiro envio do Projeto para o Github

---
+++ src/main/java/br/com/caelum/vraptor/controller/EsqueceuController.java
@@ -0,0 +1,15 @@
+package br.com.caelum.vraptor.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+
+@Path("esqueceu")
+@Controller
+public class EsqueceuController {
+
+	@Get("")
+	public void esqueceu() {
+		
+	}
+}
